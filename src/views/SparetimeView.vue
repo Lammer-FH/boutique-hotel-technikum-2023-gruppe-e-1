026314917f3163bed69016737e72bf1b1cea9a67
Stefanie Haindl <11haindl@gmail.com>
2023-10-20 23:08:55 +0200

    Content for sparetime page

--- src/views/SparetimeView.vue
+++ src/views/SparetimeView.vue
@@ -6,6 +6,28 @@ export default {
 
 <template>
 <h1>Freizeit</h1>
+
+  <p>
+    Ihr Aufenthalt in Boutique Hotel geht über die luxuriösen Zimmer und erstklassigen Service hinaus, denn wir bieten eine breite Palette von Freizeitaktivitäten, um sicherzustellen, dass Ihre Zeit bei uns unvergesslich wird.
+  </p>
+  <p>
+    Für diejenigen, die Entspannung und Erholung suchen, steht unser Wellnessbereich zur Verfügung. Tauchen Sie ein in eine Oase der Ruhe und lassen Sie sich von erfahrenen Masseuren und Therapeuten verwöhnen. Unsere Sauna und Whirlpool bieten die perfekte Entspannung, um den Alltagsstress hinter sich zu lassen.
+  </p>
+  <p>
+    Aktivurlauber werden unsere Fitness-Einrichtungen zu schätzen wissen. Unsere topmodernen Geräte und professionellen Trainer helfen Ihnen dabei, in Form zu bleiben, während Sie Ihren Aufenthalt genießen. Wir bieten auch geführte Fitnesskurse und Aktivitäten im Freien, um die malerische Umgebung zu erkunden.
+  </p>
+  <p>
+    Wenn Sie die Natur lieben, haben wir verschiedene Outdoor-Aktivitäten im Angebot. Von Wandern und Radfahren in der schönen Umgebung bis hin zu Aktivitäten wie Angeln oder Wassersport, es gibt für jeden etwas zu entdecken.
+  </p>
+  <p>
+    Für diejenigen, die Kultur und Unterhaltung suchen, organisieren wir regelmäßig Veranstaltungen und Vorführungen. Informieren Sie sich an der Rezeption über kommende Konzerte, Ausstellungen und andere kulturelle Highlights in der Umgebung.
+  </p>
+  <p>
+    Kinder sind bei uns ebenfalls herzlich willkommen. Unser Kinderclub sorgt dafür, dass die jüngsten Gäste Spaß und Unterhaltung haben, während die Eltern entspannen.
+  </p>
+  <p>
+    Wir möchten sicherstellen, dass Ihr Aufenthalt im Boutique Hotel nicht nur komfortabel, sondern auch unterhaltsam ist. Unsere Freizeitaktivitäten bieten Abwechslung und machen Ihren Besuch zu einem rundum gelungenen Erlebnis.
+  </p>
 </template>
 
 <style scoped>
