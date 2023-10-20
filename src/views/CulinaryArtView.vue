43ea3b83b137242880590c8672934c8337495160
Stefanie Haindl <11haindl@gmail.com>
2023-10-20 23:06:10 +0200

    Content for culinary art page

--- src/views/CulinaryArtView.vue
+++ src/views/CulinaryArtView.vue
@@ -5,7 +5,23 @@ export default {
 </script>
 
 <template>
-<h1>Kulinarik</h1>
+<h2>Lassen Sie sich kulinarisch verwöhnen</h2>
+
+  <p>
+    In unserem Hotel legen wir größten Wert auf ein kulinarisches Erlebnis, das die Sinne begeistert und Gaumenfreuden auf höchstem Niveau bietet. Unser hauseigenes Restaurant ist ein Schmelztiegel der Aromen und Genüsse, wo unsere talentierten Küchenchefs mit Leidenschaft und Kreativität arbeiten, um Ihnen unvergessliche Mahlzeiten zu servieren.
+  </p>
+  <p>
+    Unsere Speisekarte ist sorgfältig zusammengestellt und spiegelt die Vielfalt der internationalen und regionalen Küche wider. Von exquisiten Gourmetgerichten bis hin zu authentischen lokalen Spezialitäten – in unserem Restaurant ist für jeden Geschmack etwas dabei. Wir legen großen Wert auf frische, hochwertige Zutaten und arbeiten eng mit lokalen Bauern und Lieferanten zusammen, um sicherzustellen, dass jedes Gericht ein Geschmackserlebnis ist.
+  </p>
+  <p>
+    Ob Sie ein romantisches Abendessen zu zweit, eine Geschäftsbesprechung oder eine Feierlichkeit planen, unser Restaurant bietet die ideale Atmosphäre. Unser aufmerksames Servicepersonal steht Ihnen jederzeit zur Verfügung, um Ihren Wünschen gerecht zu werden und Empfehlungen zu geben.
+  </p>
+  <p>
+    Zusätzlich zu unserem Restaurant bieten wir auch eine gemütliche Lounge oder Bar, wo Sie erlesene Weine, handgefertigte Cocktails und eine Auswahl an Snacks genießen können.
+  </p>
+  <p>
+    Wir sind stolz darauf, dass die Kulinarik in unserem Hotel nicht nur eine Mahlzeit ist, sondern ein unvergessliches Erlebnis, das Ihren Aufenthalt bereichert. Lassen Sie sich von uns verwöhnen und entdecken Sie, wie Essen im Boutique Hotel zu einer Reise für die Sinne wird.
+  </p>
 </template>
 
 <style scoped>
