ab577dd0d44e7386be90b4e9791d59eae0257beb
Stefanie Haindl <11haindl@gmail.com>
2023-10-20 23:32:40 +0200

    Content for About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="about">
-    <h1>ABOUT</h1>
-  </div>
+    <h2>Unsere Geschichte</h2>
+
+    <p>
+    In den frühen 1920er Jahren, als die Welt von Veränderungen und Entdeckungen erfüllt war, entstand eine Vision, die weit über die Grenzen der Zeit hinausreichen sollte. Das Boutique Hotel, ein zeitloses Juwel inmitten einer malerischen Landschaft, fand seinen Ursprung in den Träumen und Ambitionen eines Mannes, Mr. John Thornton, einem visionären Unternehmer und leidenschaftlichen Reisenden.
+    </p>
+    <img src="https://media.istockphoto.com/id/157419633/de/foto/gesch%C3%A4ftsmann-in-1919-black-und-wei%C3%9F.jpg?s=1024x1024&w=is&k=20&c=iHY13bo1vGbIeCwsNCzVeXHmBuwmACD-MPi0Fyr7RE8=" class="d-block w-100">
+
+    <p>
+    Thornton hatte eine Faszination für Schönheit und Gastfreundschaft, die er während seiner Reisen rund um den Globus sammelte. Diese Eindrücke inspirierten ihn dazu, ein Hotel zu schaffen, das den Charme der Vergangenheit mit dem Komfort und der Eleganz der Moderne verband. Mit unermüdlichem Engagement und Unterstützung von Gleichgesinnten erbaute er das Boutique Hotel als einen Ort, an dem Geschichten geschrieben und Erinnerungen geschaffen werden sollten.
+    </p>
+    <p>
+    Die Einrichtung des Boutique Hotels ist ein lebendiges Zeugnis von Thorntons Liebe zur Kunst und Kultur. Die elegante Architektur, die fein gearbeiteten Details und die exquisiten Antiquitäten schaffen eine Atmosphäre, die die Gäste in eine andere Zeit versetzt.
+    </p>
+    <p>
+    Thorntons Team, angeführt von seiner Tochter, Emily Thornton, teilte seine Leidenschaft für Gastfreundschaft und das Streben nach Perfektion. Unter ihrer Führung wurde das Boutique Hotel zu einem Ort, an dem Gäste nicht nur willkommen, sondern wie alte Freunde behandelt werden. Das Team ist eine eingeschworene Gemeinschaft von Menschen, die sich der Erfüllung der Vision des Boutique Hotels verschrieben haben.
+    </p>
+    <p>
+    Unsere Hotelangestellten sind mehr als nur Mitarbeiter – sie sind Geschichtenerzähler, Lebenskünstler und Botschafter einer lange vergangenen Zeit. Ihr Engagement für exzellenten Service und ihre Liebe zum Detail machen das Boutique Hotel zu einem Ort, an dem jeder Gast wie ein König behandelt wird.
+    </p>
+    <p>
+    Die Geschichte des Boutique Hotels und die Menschen, die sie gestalten, sind untrennbar miteinander verbunden. Zusammen schaffen sie eine Oase der Ruhe und Eleganz, die es den Gästen ermöglicht, die Hektik des Alltags zu vergessen und in die zeitlose Schönheit und Wärme des Boutique Hotels einzutauchen."
+    </p>
+
 </template>
 
 <style>
@@ -13,3 +33,6 @@
   }
 }
 </style>
+<script setup>
+import PersonalDataView from "@/views/PersonalDataView.vue";
+</script>
