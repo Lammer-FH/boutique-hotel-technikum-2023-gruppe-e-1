d7c8e12b3a045ec7cc979fdc301121f783acb985
Stefanie Haindl <11haindl@gmail.com>
2023-10-22 11:24:13 +0200

    removed comment

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -9,7 +9,6 @@ export default {
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img src="https://picsum.photos/id/202/200/300" class="d-block w-100">
-        <!--<svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="https://picsum.photos/id/202/200/300" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#555" dy=".3em">First slide</text></svg>-->
       </div>
       <div class="carousel-item">
         <img src="https://picsum.photos/id/203/200/300" class="d-block w-100">
