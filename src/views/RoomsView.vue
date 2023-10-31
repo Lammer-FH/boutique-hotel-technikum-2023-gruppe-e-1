081675786ccfa2614967edc315636a1cc884cf42
clara <110772209+Aentoni@users.noreply.github.com>
2023-10-31 18:16:43 +0100

    getRooms Api added

--- src/views/RoomsView.vue
+++ src/views/RoomsView.vue
@@ -1,30 +1,57 @@
 <script>
 import RoomCard from "@/components/RoomCard.vue";
 
+import axios from 'axios'
+
 export default {
   name: "RoomsView",
   components: {RoomCard},
+
   data() {
     return {
       rooms: [
         {
-          roomName: "Zimmer klein", price: "100 €", imagePath: "https://images.unsplash.com/photo-1618773928121-c32242e63f39?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          roomName: "Zimmer klein",
+          price: "100 €",
+          imagePath: "https://images.unsplash.com/photo-1618773928121-c32242e63f39?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
-          roomName: "Zimmer groß", price: "150 €", imagePath: "https://plus.unsplash.com/premium_photo-1670360414903-19e5832f8bc4?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          roomName: "Zimmer groß",
+          price: "150 €",
+          imagePath: "https://plus.unsplash.com/premium_photo-1670360414903-19e5832f8bc4?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
-          roomName: "Suit", price: "250 €", imagePath: "https://images.unsplash.com/photo-1631049552057-403cdb8f0658?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          roomName: "Suit",
+          price: "250 €",
+          imagePath: "https://images.unsplash.com/photo-1631049552057-403cdb8f0658?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
       ]
+    }
+  },
+
+  methods: {
+    getRooms() {
+      axios.get("https://boutique-hotel.helmuth-lammer.at/api/v1/rooms")
+          .then(response => {
+          //  let data = response.data
+            console.log(response)
+          })
+          .catch(error => {
+            // handle error
+            console.log(error)
+          })
+          .then(() => {
+            // always executed
+          });
     }
   }
 }
 </script>
 
 <template>
-<h2>Unsere Zimmer</h2>
-  <RoomCard v-for="room in rooms" :roomName=room.roomName :price=room.price :imagePath=room.imagePath />
+  <h2>Unsere Zimmer</h2>
+  <RoomCard @click="getRooms()" v-for="room in rooms" :roomName=room.roomName :price=room.price :imagePath=room.imagePath />
+
 </template>
 
 <style scoped>
