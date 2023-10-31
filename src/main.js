9b763be5712d843f2e4d2b393898d6bb8e40df64
MGroegerAT <118934929+MGroegerAT@users.noreply.github.com>
2023-10-31 19:36:09 +0100

    changed to bootstrap-vue-next

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
-import BootstrapVue3 from 'bootstrap-vue-3'
+import BootstrapVueNext from 'bootstrap-vue-next'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -13,6 +13,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(BootstrapVue3)
+app.use(BootstrapVueNext)
 
 app.mount('#app')
