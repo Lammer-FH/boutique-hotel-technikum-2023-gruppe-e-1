5d8a2c7a6d740206405268b79d6a9a4b71a262d6
Stefanie Haindl <11haindl@gmail.com>
2023-11-01 14:59:08 +0100

    changed carousel to bootstrap-vue-next

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -1,33 +1,15 @@
 <script>
 export default {
-  name: "ImageCarousel"
-}
+  name: "ImageCarousel",
+};
 </script>
 
 <template>
-  <div id="carouselExample" class="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="https://picsum.photos/id/202/200/300" class="d-block w-100">
-      </div>
-      <div class="carousel-item">
-        <img src="https://picsum.photos/id/203/200/300" class="d-block w-100">
-      </div>
-      <div class="carousel-item">
-        <img src="https://picsum.photos/id/351/200/300" class="d-block w-100">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
+  <BCarousel controls indicators ride="carousel">
+    <BCarouselSlide img-src="https://picsum.photos/id/202/200/300" />
+    <BCarouselSlide img-src="https://picsum.photos/id/203/200/300" />
+    <BCarouselSlide img-src="https://picsum.photos/id/351/200/300" />
+  </BCarousel>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
