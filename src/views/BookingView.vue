ead9e7e8d6d82499062508d1908fda18292a131d
Stefanie Haindl <11haindl@gmail.com>
2023-11-01 14:59:45 +0100

    added Accordion for booking process

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -5,7 +5,40 @@ export default {
 </script>
 
 <template>
-<h1>Buchen</h1>
+<BAccordion>
+  <BAccordionItem title="1. Verfügbarkeit prüfen" visible>
+    <strong>This is the first item's accordion body.</strong> It is shown by default, until the
+    collapse plugin adds the appropriate classes that we use to style each element. These classes
+    control the overall appearance, as well as the showing and hiding via CSS transitions. You can
+    modify any of this with custom CSS or overriding our default variables. It is also worth noting
+    that just about any HTML can go within the <code>.accordion-body</code>, though the transition
+    does limit overflow.
+  </BAccordionItem>
+  <BAccordionItem title="2. Zimmer auswählen">
+    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the
+    collapse plugin adds the appropriate classes that we use to style each element. These classes
+    control the overall appearance, as well as the showing and hiding via CSS transitions. You can
+    modify any of this with custom CSS or overriding our default variables. It is also worth noting
+    that just about any HTML can go within the <code>.accordion-body</code>, though the transition
+    does limit overflow.
+  </BAccordionItem>
+  <BAccordionItem title="3. Kontaktdaten eingeben">
+    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the
+    collapse plugin adds the appropriate classes that we use to style each element. These classes
+    control the overall appearance, as well as the showing and hiding via CSS transitions. You can
+    modify any of this with custom CSS or overriding our default variables. It is also worth noting
+    that just about any HTML can go within the <code>.accordion-body</code>, though the transition
+    does limit overflow.
+  </BAccordionItem>
+  <BAccordionItem title="4. Buchung abschließen">
+    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the
+    collapse plugin adds the appropriate classes that we use to style each element. These classes
+    control the overall appearance, as well as the showing and hiding via CSS transitions. You can
+    modify any of this with custom CSS or overriding our default variables. It is also worth noting
+    that just about any HTML can go within the <code>.accordion-body</code>, though the transition
+    does limit overflow.
+  </BAccordionItem>
+</BAccordion>
 </template>
 
 <style scoped>
