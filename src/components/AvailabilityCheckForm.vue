d4caee3bdabf55b42a1847d4911dcfa01a79ae3e
Stefanie Haindl <11haindl@gmail.com>
2023-11-03 15:25:28 +0100

    gui for availability check form

---
+++ src/components/AvailabilityCheckForm.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name: "AvailabilityCheckForm",
+};
+</script>
+
+<template>
+  <div class="mb-3">
+    <label for="date-from" class="form-label"
+      >Anreisedatum:</label
+    >
+    <input
+      type="date"
+      class="form-control"
+      id="date-from"
+    />
+  </div>
+  <div class="mb-3">
+    <label for="date-to" class="form-label"
+      >Abreisedatum:</label
+    >
+    <input
+      type="date"
+      class="form-control"
+      id="date-to"
+    />
+  </div>
+  <div class="mb-3">
+    <label for="number-of-persons" class="form-label"
+      >Anzahl der Personen:</label
+    >
+    <input
+      type="number"
+      class="form-control"
+      id="number-of-persons"
+    />
+  </div>
+  <div class="d-grid gap-2">
+    <button type="submit" class="btn btn-primary ">Verfügbarkeit prüfen</button>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -1,6 +1,9 @@
 <script>
+import AvailabilityCheckForm from '../components/AvailabilityCheckForm.vue';
+
 export default {
-  name: "BookingView",
+    name: "BookingView",
+    components: { AvailabilityCheckForm }
 };
 </script>
 
@@ -25,18 +28,11 @@ export default {
       </h2>
       <div
         id="collapseOne"
-        class="accordion-collapse collapse"
+        class="accordion-collapse collapse show"
         data-bs-parent="#booking-accordion"
       >
         <div class="accordion-body">
-          <strong>This is the first item's accordion body.</strong> It is shown
-          by default, until the collapse plugin adds the appropriate classes
-          that we use to style each element. These classes control the overall
-          appearance, as well as the showing and hiding via CSS transitions. You
-          can modify any of this with custom CSS or overriding our default
-          variables. It's also worth noting that just about any HTML can go
-          within the <code>.accordion-body</code>, though the transition does
-          limit overflow.
+          <AvailabilityCheckForm/>
         </div>
       </div>
     </div>
