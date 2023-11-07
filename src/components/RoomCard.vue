43670ec0fc48bc9d5d3056b45deecdeed8d54a4c
clara <110772209+Aentoni@users.noreply.github.com>
2023-11-07 17:46:47 +0100

    axios call getRooms funktioniert, in Card werden Name, Preis angezeigt, pager eingefügt, es fehlt: Bilder Zimmer und dynamisch anzeigen, Pager noch nicht fertig

--- src/components/RoomCard.vue
+++ src/components/RoomCard.vue
@@ -1,25 +1,22 @@
 <script>
 export default {
   name: "RoomCard",
-  props: ["roomName", "price", "imagePath"],
+  props: ["id", "roomName", "price"],
 }
 
 </script>
 
 <template>
-    <b-card
-        :title="roomName"
-        :img-src="imagePath"
-        img-alt="Image"
-        img-top
-        tag="article"
-        style="max-width: 20rem;"
-        class="mb-2"
-    >
-      <b-card-text>
-        aktueller Preis: <b>{{price}}</b>
-      </b-card-text>
-    </b-card>
+
+  <!-- klassisches Bootstrap verwendet, weil mit der Bootstrap-Vue Card das Bild nicht angezeigt wurde -->
+  <div class="card" style="width: 18rem;">
+    <img src="../assets/images/rooms/1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">{{this.roomName}}</h5>
+      <p class="card-text"> aktueller Preis: <b>{{ price }}</b></p>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
