dbd456c548a6025529db61d1fead1b78d66dcad1
clara <110772209+Aentoni@users.noreply.github.com>
2023-11-14 21:29:53 +0100

    new file RoomDetailsView.vue,new path (router) to roomDetailsView, images added

--- src/components/RoomCard.vue
+++ src/components/RoomCard.vue
@@ -1,21 +1,24 @@
 <script>
 export default {
   name: "RoomCard",
-  props: ["id", "roomName", "price"],
-}
+  props: ["room"],
 
+}
 </script>
 
 <template>
 
   <!-- klassisches Bootstrap verwendet, weil mit der Bootstrap-Vue Card das Bild nicht angezeigt wurde -->
-  <div class="card" style="width: 18rem;">
-    <img src="../assets/images/rooms/1.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">{{this.roomName}}</h5>
-      <p class="card-text"> aktueller Preis: <b>{{ price }}</b></p>
+
+  <RouterLink :to="'/roomDetails/' + this.room.id">
+    <div class="card" style="width: 18rem;">
+      <img src="../assets/images/rooms/1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">{{ this.room.roomsName }}</h5>
+        <p class="card-text"> aktueller Preis: <b>{{ this.room.pricePerNight }}</b></p>
+      </div>
     </div>
-  </div>
+  </RouterLink>
 
 </template>
 
---
+++ src/views/RoomDetailsView.vue
@@ -0,0 +1,28 @@
+<script>
+
+export default {
+  name: "RoomDetailsView",
+
+  data() {
+    return {
+      roomId: 1
+    }
+  },
+
+  created() {
+    this.roomId = this.$route.params.roomId
+    console.log(this.roomId)
+  }
+
+}
+
+</script>
+
+<template>
+  <h2>Unsere Zimmer mit Details</h2>
+
+</template>
+
+<style scoped>
+
+</style>
