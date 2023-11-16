fcb983c9d3537bb92b9a86186d2ebb4fd634d9cd
clara <karrotte@protonmail.com>
2023-11-16 21:51:06 +0100

    extras added in RoomDetailsView, image and icons are still missing

--- src/main.js
+++ src/main.js
@@ -3,16 +3,22 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 import BootstrapVueNext from 'bootstrap-vue-next'
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {faUserSecret} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(BootstrapVueNext)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
--- src/views/RoomDetailsView.vue
+++ src/views/RoomDetailsView.vue
@@ -1,9 +1,11 @@
 <script>
 
 import axios from "axios";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
 export default {
   name: "RoomDetailsView",
+  components: {FontAwesomeIcon},
 
   data() {
     return {
@@ -27,6 +29,31 @@ export default {
           .catch(error => {
             console.error(error);
           });
+    },
+
+    getIconClass(extraName) {
+      const iconMap = {
+        'minibar': 'fa fa-glass-martini',
+        'bathroom': 'fa fa-bath',
+
+        // Add other mappings as needed
+      };
+      return iconMap[extraName] || 'fa fa-question-circle'; // default icon
+    },
+
+    getNameToDisplay(nameFromDatabase) {
+      const nameMap = {
+        'bathroom': 'Badezimmer',
+        'minibar': 'Minibar',
+        'television': 'Fernseher',
+        'livingroom': 'Wohnzimmer',
+        'aircondition': 'Klimaanlage',
+        'wifi': 'WLAN',
+        'breakfast': 'Frühstück',
+        'handicapped accessible': 'behindertengerecht'
+        // Add other mappings as needed
+      };
+      return nameMap[nameFromDatabase] || nameFromDatabase; // return the internal name if no mapping is found
     }
   }
 }
@@ -35,7 +62,19 @@ export default {
 
 <template>
   <div v-if="room">
-  <h2>{{ room.roomName }}</h2>
+    <h2>{{ room.roomName }}</h2>
+    <font-awesome-icon icon="fa-solid fa-user-secret"/>
+
+    <ul>
+      <li>Anzahl Betten: {{ room.beds }}</li>
+      <li v-for="extra in room.extras" :key="extra.id">
+
+        <i :class="getIconClass(extra.name)"></i>
+
+        {{ getNameToDisplay(extra.name) }}: {{ extra.available === 1 ? 'ja' : 'nein' }}
+      </li>
+    </ul>
+
 
   </div>
 
