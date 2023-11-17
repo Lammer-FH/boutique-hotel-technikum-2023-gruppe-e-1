04665c4b9047c8b6af87a9e1d0b0dba7d225ac3f
clara <karrotte@protonmail.com>
2023-11-17 12:07:57 +0100

    images added to RoomDetailsView.vue, has to be styled!

--- src/views/RoomDetailsView.vue
+++ src/views/RoomDetailsView.vue
@@ -2,6 +2,7 @@
 
 import axios from "axios";
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {ref} from "vue";
 
 export default {
   name: "RoomDetailsView",
@@ -9,11 +10,39 @@ export default {
 
   data() {
     return {
-      room: null
+      room: null,
+      roomId: null,
+      currentImageIndex: ref(0),
+      // Assuming room details are fetched and stored in `room`
+      imageSources: [],
+
     }
   },
 
+  watch: {
+    async room(newRoom) {
+      if (newRoom && newRoom.id) {
+        try {
+          const imagePath = await import(`@/assets/images/rooms/${newRoom.id}.jpg`);
+          this.imageSources = [imagePath.default];
+        } catch (error) {
+          console.error('Error loading image:', error);
+          // Handle the error or set a default image
+        }
+      }
+    }
+  },
+
+
+  computed: {
+    imagePath() {
+      return this.imageSources.length > 0 ? this.imageSources[this.currentImageIndex] : '';
+    }
+  },
+
+
   created() {
+    this.roomId = this.$route.params.roomId;
     this.fetchRoomDetails();
   },
 
@@ -69,6 +98,8 @@ export default {
 <template>
   <div v-if="room">
     <h3>{{ room.roomName }}</h3>
+
+    <img :src="imagePath" class="room-image" alt="Hotelzimmer">
 <br>
     <p>Das Zimmer ist mit folgenden Extras ausgestattet:</p>
     <ul class="list-unstyled">
