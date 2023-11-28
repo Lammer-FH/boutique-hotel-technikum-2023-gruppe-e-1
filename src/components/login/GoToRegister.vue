bbb20a505b5bb6f2a5a05b80bda81f1978cc1557
Stefanie Haindl <11haindl@gmail.com>
2023-11-28 19:26:07 +0100

    Design for Overview Login and Registration

---
+++ src/components/login/GoToRegister.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "GoToRegistration",
+};
+</script>
+
+<template>
+  <div
+    class="col-sm-6 d-flex justify-content-center text-center align-items-center"
+  >
+    <div class="row ml-3">
+      <div class="col m-3">
+        <label for="register-button" class="form-label"
+          >Noch nicht registriert?</label
+        ><br />
+        <button
+          id="register-button"
+          type="button"
+          class="btn btn-primary"
+        >
+          Registrieren
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/login/LoginForm.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: "LoginForm",
+};
+</script>
+
+<template>
+  <div class="col-sm-6 border border-top-0 border-start-0 border-bottom-0">
+    <div class="row mr-3">
+      <div class="col-sm">
+        <label for="email" class="form-label">E-Mailadresse</label>
+        <input id="email" name="email" type="email" class="form-control mb-3" />
+        <div class="form-text" id="email-message"></div>
+      </div>
+    </div>
+    <div class="row mr-3">
+      <div class="col-sm">
+        <label for="password" class="form-label">Passwort</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          class="form-control mb-3"
+        />
+        <div class="form-text" id="password-message"></div>
+      </div>
+    </div>
+    <div class="row mr-3">
+      <div class="col-sm d-grid">
+        <button id="login-button" type="button" class="btn btn-primary">
+          Anmelden
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
