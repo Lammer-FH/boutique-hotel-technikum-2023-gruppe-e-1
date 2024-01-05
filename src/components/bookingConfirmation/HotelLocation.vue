6ad6ecaf37c4ccd2020b8f1a264039ba87b37744
MGroegerAT <118934929+MGroegerAT@users.noreply.github.com>
2024-01-05 17:39:11 +0100

    map added to bookingConfirmation.vue

--- src/components/bookingConfirmation/HotelLocation.vue
+++ src/components/bookingConfirmation/HotelLocation.vue
@@ -1,51 +1,73 @@
 <script>
-import GoogleMapsApiLoader from 'google-maps-api-loader'
+import {defineComponent} from "vue";
+import {GoogleMap, Marker} from "vue3-google-map";
+
 
 export default {
   name: "HotelLocation",
-  props: {
-    mapConfig: Object,
-    apiKey: String,
-  },
-
+  components: { GoogleMap, Marker },
   data() {
     return {
-      google: null,
-      map: null
+      mapCenter: {lat: 48.208492, lng: 16.373127},
+      currentLocation: {
+        position: {lat: 0, lng: 0},
+        label: "Me"
+      },
+      hotelLocation: {
+        position: {lat: 48.208492, lng: 16.373127},
+        label: 'BH'
+      }
     }
-  },
 
-  async mounted() {
-    const googleMapApi = await GoogleMapsApiLoader({
-      apiKey: this.apiKey
-    })
-    this.google = googleMapApi
-    this.initializeMap()
   },
 
   methods: {
-    initializeMap() {
-      const mapContainer = this.$refs.googleMap
-      this.map = new this.google.maps.Map(
-          mapContainer, this.mapConfig
-      )
+    async getCurrentLocation() {
+      try {
+        // Use Geolocation API to get the current location
+        const position = await new Promise((resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+
+        // Update the currentLocation property with the obtained coordinates
+        this.currentLocation.position = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        console.log(this.currentLocation)
+      } catch (error) {
+        console.error("Error getting current location:", error);
+        // Handle errors here, for example, notify the user or set default values
+      }
     }
+  },
+  mounted() {
+    // Call the method to get the current location when the component is mounted
+    this.getCurrentLocation();
   }
-
-
 }
 </script>
 
 <template>
-  <div>
-    <div class="google-map" ref="googleMap"></div>
-    <template v-if="Boolean(this.google) && Boolean(this.map)">
-      <slot
-          :google="google"
-          :map="map"
-      />
-    </template>
-  </div>
+  <p>Anfahrt</p>
+  <GoogleMap
+      api-key="AIzaSyBd9hZJusA-siF_MtwHHnIRwYwidRrtUL0"
+      style="width: 100%; height: 500px"
+      :center="mapCenter"
+      :zoom="15"
+  >
+    <Marker :options="hotelLocation" />
+    <Marker :options="currentLocation" />
+
+    <GmapDirectionsRenderer
+        :origin="currentLocation"
+        :destination="hotelLocation"
+        :travel-mode="'DRIVING'"
+        :options="{draggable: true}"
+    ></GmapDirectionsRenderer>
+
+
+  </GoogleMap>
 </template>
 
 <style scoped>
