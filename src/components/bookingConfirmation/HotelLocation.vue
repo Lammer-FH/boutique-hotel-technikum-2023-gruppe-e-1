be49ae53d1630db9281c1203680d5f246ce78649
MGroegerAT <118934929+MGroegerAT@users.noreply.github.com>
2024-01-06 13:08:25 +0100

    maps directions added

--- src/components/bookingConfirmation/HotelLocation.vue
+++ src/components/bookingConfirmation/HotelLocation.vue
@@ -1,75 +1,80 @@
+<template>
+  <div>
+    <div ref="mapRef" style="width: 800px; height: 600px"></div>
+    <select v-model="travelMode">
+      <option value="DRIVING">Car</option>
+      <option value="TRANSIT">Public Transportation</option>
+      <option value="BICYCLING">Bicycle</option>
+      <option value="WALKING">Walking</option>
+    </select>
+  </div>
+</template>
+
 <script>
-import {defineComponent} from "vue";
-import {GoogleMap, Marker} from "vue3-google-map";
-
-
 export default {
-  name: "HotelLocation",
-  components: { GoogleMap, Marker },
   data() {
     return {
+      map: null,
+      markerA: null,
+      markerB: null,
+      directionsService: null,
+      directionsRenderer: null,
       mapCenter: {lat: 48.208492, lng: 16.373127},
-      currentLocation: {
-        position: {lat: 0, lng: 0},
-        label: "Me"
-      },
-      hotelLocation: {
-        position: {lat: 48.208492, lng: 16.373127},
-        label: 'BH'
-      }
+      currentLocation: { lat: 0, lng: 0 },
+      hotelLocation: { lat: 48.208492, lng: 16.373127 },
+      travelMode: 'DRIVING',
     }
-
   },
-
-  methods: {
-    async getCurrentLocation() {
-      try {
-        // Use Geolocation API to get the current location
-        const position = await new Promise((resolve, reject) => {
-          navigator.geolocation.getCurrentPosition(resolve, reject);
-        });
-
-        // Update the currentLocation property with the obtained coordinates
-        this.currentLocation.position = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        console.log(this.currentLocation)
-      } catch (error) {
-        console.error("Error getting current location:", error);
-        // Handle errors here, for example, notify the user or set default values
-      }
+  watch: {
+    travelMode(newValue) {
+      // Re-draw the route when the travel mode changes
+      this.drawRoute();
     }
   },
   mounted() {
-    // Call the method to get the current location when the component is mounted
+    this.initializeMap();
     this.getCurrentLocation();
-  }
+  },
+  methods: {
+    initializeMap() {
+      this.map = new google.maps.Map(this.$refs.mapRef, {
+        center: this.mapCenter,
+        zoom: 8
+      });
+
+      this.directionsService = new google.maps.DirectionsService();
+      this.directionsRenderer = new google.maps.DirectionsRenderer();
+      this.directionsRenderer.setMap(this.map);
+    },
+    getCurrentLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          this.currentLocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          this.drawRoute();
+        });
+      } else {
+        alert("Geolocation is not supported by this browser.");
+      }
+    },
+    drawRoute() {
+      this.directionsService.route(
+          {
+            origin: this.currentLocation,
+            destination: this.hotelLocation,
+            travelMode: google.maps.TravelMode[this.travelMode]
+          },
+          (result, status) => {
+            if (status === "OK") {
+              this.directionsRenderer.setDirections(result);
+            } else {
+              alert("Directions request failed due to " + status);
+            }
+          }
+      );
+    },
+  },
 }
 </script>
-
-<template>
-  <p>Anfahrt</p>
-  <GoogleMap
-      api-key="AIzaSyBd9hZJusA-siF_MtwHHnIRwYwidRrtUL0"
-      style="width: 100%; height: 500px"
-      :center="mapCenter"
-      :zoom="15"
-  >
-    <Marker :options="hotelLocation" />
-    <Marker :options="currentLocation" />
-
-    <GmapDirectionsRenderer
-        :origin="currentLocation"
-        :destination="hotelLocation"
-        :travel-mode="'DRIVING'"
-        :options="{draggable: true}"
-    ></GmapDirectionsRenderer>
-
-
-  </GoogleMap>
-</template>
-
-<style scoped>
-
-</style>
